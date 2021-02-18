2a047a9f613513e48e42fbbf70097b70d603a388
younglaecho <tks7205@gmail.com>
2021-02-19 02:07:06 +0900

    make it better

--- company/settings.py
+++ company/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'jhqb&=a-a1jfrvc&)3#))h)^=))nue)%0c3@r(mr-2jkfoed7!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'company.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
