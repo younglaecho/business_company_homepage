d17f7613efe545a56901343987c6e27baf5ce68d
younglaecho <tks7205@gmail.com>
2021-03-12 01:54:06 +0900

    modify mainimage to fade effect

--- static/index.js
+++ static/index.js
@@ -5,6 +5,43 @@ const fixedNavItems = document.querySelector('.fixed-nav__items');
 const Body = document.querySelector('body');
 const mainMenuItems = document.querySelector('.main__menu__items')
 
+
+let index = 1
+
+const mainImage = document.querySelector('.main__slide-contents')
+const mainImage1 = document.querySelector('.main__sliding-image1')
+const mainImage2 = document.querySelector('.main__sliding-image2')
+const mainImage3 = document.querySelector('.main__sliding-image3')
+const changeImage = function() {
+    // console.log('time')
+    if (index== 1) {
+        mainImage1.style.display = 0
+        mainImage2.style.opacity = 1
+        mainImage3.style.opacity = 0
+    }
+    if (index== 2) {
+        mainImage1.style.opacity = 0
+        mainImage2.style.opacity = 0
+        mainImage3.style.opacity = 1
+    }
+    if (index== 3) {
+        mainImage1.style.opacity = 1
+        mainImage2.style.opacity = 0
+        mainImage3.style.opacity = 0
+        index = 0
+    }
+    // mainImage.style.transform = "translateX("+(-100*index)+"vw)"
+
+    // if (index==3) {
+    //     mainImage.style.transform = "translateX(0vw)"
+    //     index = 0
+    // }
+    index = index + 1
+}
+
+let fadeTime = setInterval(changeImage,5000)
+// let fadeTime = setTimeout(changeImage,1000)
+
 // function dropDownMenu (event, index, text) {
 //     const dropDown1 = document.createElement('ul');
 //     dropDown1.setAttribute('class', 'drop-down');
@@ -19,19 +56,17 @@ const mainMenuItems = document.querySelector('.main__menu__items')
 //         const dropDown1 = document.querySelector('.drop-Down')
 //         dropDown1 && dropDown1.remove()
 //     });
-
 // }
 window.addEventListener('scroll', () => {
-    if (window.scrollY==0) {
-        fixedNav.style.top = 30 +'px';
-    } else {
-        fixedNav.style.top = 0;
-    }
+    // if (window.scrollY==0) {
+    //     fixedNav.style.top = 30 +'px';
+    // } else {
+    //     fixedNav.style.top = 0;
+    // }
     if ((window.scrollY + window.innerHeight) > mainMenuItems.getBoundingClientRect().top+100) {
         mainMenuItems.style.transform = 'translateY(-20px)'
         mainMenuItems.style.opacity = '1'
         mainMenuItems.style.transition = 'all ease-in 0.3s';
-
     }
 })
 
@@ -57,5 +92,29 @@ window.addEventListener('scroll', () => {
 //         dropDownMenu(event, index, innerHTML)
 //     }
 // })
+const scrollUnit = window.innerHeight * 6/ 7
+const toggleButton1 = document.querySelector('.toggle__button1') 
+const toggleButton2 = document.querySelector('.toggle__button2') 
+const toggleButton3 = document.querySelector('.toggle__button3')
 
+toggleButton1.addEventListener('click', () => {
+    window.scrollTo({
+        top:0,
+        behavior: 'smooth'
+    })
+})
+
+toggleButton2.addEventListener('click', () => {
+    window.scrollTo({
+        top:  scrollUnit,
+        behavior: 'smooth'
+    })
+})
+
+toggleButton3.addEventListener('click', () => {
+    window.scrollTo({
+        top: scrollUnit*2,
+        behavior: 'smooth'
+    })
+})
 
