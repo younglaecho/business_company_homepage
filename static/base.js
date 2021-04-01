e41713249564c5a89e1c7f10bdc6120628bda74c
younglaecho <tks7205@gmail.com>
2021-04-02 00:29:52 +0900

    business page design modify

--- static/base.js
+++ static/base.js
@@ -1,15 +1,15 @@
-const menuList = document.querySelector("#fiexed-nav__items")
-const menuItem1 = document.querySelector("#fiexed-nav__menu1")
-const menuItem2 = document.querySelector("#fiexed-nav__menu2")
-const menuItem3 = document.querySelector("#fiexed-nav__menu3")
-const menuItem4 = document.querySelector("#fiexed-nav__menu4")
-const menuItem5 = document.querySelector("#fiexed-nav__menu5")
+const menuList = document.querySelector('#fixed-nav__items1');
+const menuItem1 = document.querySelector("#fixed-nav__menu1");
+const menuItem2 = document.querySelector("#fixed-nav__menu2");
+const menuItem3 = document.querySelector("#fixed-nav__menu3");
+const menuItem4 = document.querySelector("#fixed-nav__menu4");
+const menuItem5 = document.querySelector("#fixed-nav__menu5");
 
+console.log(menuList)
 const LinktoPage = function (event) {
     if (document.body.clientWidth<720) {
         
     } else if (event.target==menuItem1) {
-        console.log(event.target)
         location.href = '/company/introduction/'
     } else if (event.target==menuItem2) {
         location.href = '/business/coast/'
@@ -18,9 +18,9 @@ const LinktoPage = function (event) {
     } else if (event.target==menuItem4) {
         location.href = '/board/notice/'
     } else if (event.target==menuItem5) {
-        location.href = '/recruit/'
+        location.href = '/recruit'
     }
 }
-menuList.addEventListener('click', LinktoPage(evnet))
+menuList.addEventListener('click', LinktoPage)
 
 
